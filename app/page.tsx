af2b9a33c15e6aac04b2fb06988f2054e9225e6d
John Gorriceta <johngorriceta1@gmail.com>
2024-11-08 15:48:52 -0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -18,34 +18,30 @@ export default function Home() {
         <h2 className="text-[40px] text-center md:text-left font-bold">About Me</h2>
         <p className="text-md text-center md:text-xl md:text-left">
         I&apos;m a passionate web engineer and AI/ML enthusiast studying at Brigham Young University -
-         Idaho. My primary mission is to create web AI/ML products that make a positive impact in society. 
-         I believe that technology should be a force for good, and by utilizing AI/ML, 
-         I&apos;m able to channel my energy and skills into projects that benefit people from all walks of life. 
-         Whether it&apos;s developing software that enhances accessibility, improves education, or promotes community engagement,
-          my dedication lies in harnessing the power of code to empower and uplift individuals.
+         Idaho. Lately, I've been experimenting with local LLMs, NLPs, and <Link className="underline" href={"https://www.inkandswitch.com/local-first"}>"Local-First"</Link> web architecture.
+         As cheesy as it sounds, I often feel like I'm an artist and software is my medium. I love applying new and novel concepts to the engineering that I do, so I&apos;m always exploring new things.
         </p>
       </div>
       <div className="flex flex-col justify-center items-center p-10 md:p-24">
         <h2 className="text-[40px] text-center font-bold mb-3"><span className="text-indigo-600 underline">Dev</span> ToolKit</h2>
         <p className="text-md text-center md:text-xl md:w-3/4">
-        Here are a few of my favorite tools that help me develop my projects supafast.
+        Here are a few of my favorite tools and frameworks that help me develop my projects crazy fast.
         </p>
         <div className="flex flex-row flex-wrap justify-center items-center p-5 gap-5 md:gap-10">
-          <ImageLink linkURL="https://arc.net/" imageURL="./arc.png" />
-          <ImageLink linkURL="https://code.visualstudio.com/" imageURL="./vs.png" />
-          <ImageLink linkURL="https://neovim.io/" imageURL="./Vim.png" />
-          <ImageLink linkURL="https://insomnia.rest/" imageURL="./Insomnia.png" />
-          <ImageLink linkURL="https://www.langchain.com/" imageURL="./lang.png" />
-          <ImageLink linkURL="https://www.warp.dev/" imageURL="./warp.png" />
-          <ImageLink linkURL="https://www.jetbrains.com/webstorm/" imageURL="./webstorm.png" />
+          <ImageLink linkURL="https://huggingface.co" imageURL="./hf-logo.svg" />
+          <ImageLink linkURL="https://nextjs.org" imageURL="./next.svg" />
+          <ImageLink linkURL="https://www.databricks.com" imageURL="./dblogo.png" />
+          <ImageLink linkURL="https://tailwindcss.com" imageURL="./twlogo.png" />
+          <ImageLink linkURL="https://code.visualstudio.com" imageURL="./vs.png" />
+          <ImageLink linkURL="https://neovim.io" imageURL="./Vim.png" />
+          <ImageLink linkURL="https://www.warp.dev" imageURL="./warp.png" />
         </div>
         <div className="flex flex-col p-10 md:p-24 mb-10">
           <h2 className="text-[40px] text-center font-bold mb-3">Featured Projects</h2>
           <div className="flex flex-wrap flex-row justify-center gap-5 md:gap-10">
-            <SiteLink text="Ari." href="https://learnari.com/"/>
-            <SiteLink text="Celeste" href="https://thecelesteproject.vercel.app/" />
-            <SiteLink text="Alto" href="https://alto-dating.pages.dev/"/>
-            <SiteLink text="Conn Academy" href="https://connect-academy.vercel.app/"/>
+            <SiteLink text="Munchy" href="https://munchy-murex.vercel.app"/>
+            <SiteLink text="UnBinged" href="https://unbingedtv.com/" />
+            <SiteLink text="WellWritten" href="https://wellwritten.joinpageturn.com/"/>
           </div>
         </div>
       </div>
